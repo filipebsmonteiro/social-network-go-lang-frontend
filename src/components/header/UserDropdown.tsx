be6d6609f37afc544330223c041627be225029bc
Filipe Monteiro <filipebsmonteiro@gmail.com>
2025-05-22 14:47:56 -0300

    Logout and redirect Logged user

--- src/components/header/UserDropdown.tsx
+++ src/components/header/UserDropdown.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
-import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
 import useAvatar from "../../hooks/useAvatar";
+import Button from "../ui/button/Button";
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
   const { generateAvatar } = useAvatar()
-  const { loggedUser } = useAuth();
+  const { loggedUser, signOut } = useAuth();
 
   function toggleDropdown() {
     setIsOpen(!isOpen);
@@ -139,9 +139,11 @@ export default function UserDropdown() {
             </DropdownItem>
           </li>
         </ul>
-        <Link
-          to="/signin"
-          className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
+        <Button
+          variant="outline"
+          size="xs"
+          className="py-2"
+          onClick={signOut}
         >
           <svg
             className="fill-gray-500 group-hover:fill-gray-700 dark:group-hover:fill-gray-300"
@@ -159,7 +161,7 @@ export default function UserDropdown() {
             />
           </svg>
           Sign out
-        </Link>
+        </Button>
       </Dropdown>
     </div>
   );
--- src/pages/AuthPages/SignIn.tsx
+++ src/pages/AuthPages/SignIn.tsx
@@ -1,8 +1,14 @@
 import PageMeta from "../../components/common/PageMeta";
 import AuthLayout from "./AuthPageLayout";
 import SignInForm from "../../components/auth/SignInForm";
+import useAuth from "../../hooks/useAuth";
 
 export default function SignIn() {
+  const { isAuthenticated } = useAuth();
+  if (isAuthenticated) {
+    window.location.href = "/";
+  }
+
   return (
     <>
       <PageMeta
